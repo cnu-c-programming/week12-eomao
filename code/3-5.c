6b1c38874d58e4b339d2b6516470a1d697a67536
eomao <156084917+eomao@users.noreply.github.com>
2026-05-22 12:21:52 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char* argv[]) {
     const char* filename = "numbers.txt";
@@ -8,9 +9,19 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    char buffer[64];
 
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        int number;
+        char extra;
+
+        if (sscanf(buffer, "%d %c", &number, &extra) == 1) {
+            sum += number;
+        } else {
+            fprintf(stderr, "invalid input: %s", buffer);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
