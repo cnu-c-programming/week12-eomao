9fb25a1c59905322c56bac7511e1b93738801cc6
eomao <156084917+eomao@users.noreply.github.com>
2026-05-22 11:48:22 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,23 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    if (fp == NULL)
+        return 0;
 
+    int count[26] = { 0 };
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF) {
+        if (ch >= 'a' && ch <= 'z') {
+            count[ch - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
 
     fclose(fp);
-}
 
+    return 0;
+}
