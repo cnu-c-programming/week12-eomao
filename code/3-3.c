a4c4341e045081127fd0f58a530f18fb11be1ea0
eomao <156084917+eomao@users.noreply.github.com>
2026-05-22 12:07:29 +0900

    Update 3-3.c

--- code/3-3.c
+++ code/3-3.c
@@ -3,10 +3,29 @@
 int main(int argc, const char* argv[]) {
     const char* filename = "myfile.txt";
 
+    FILE* fp1 = fopen(filename, "r");
+    FILE* fp2 = fopen(filename, "w");
 
-    "fp1 %s %p %d %d\n"
-    "fp2 %s %p %d %d\n"
-    
+    if (fp1 == NULL || fp2 == NULL) {
+        return 0;
+    }
+
+    char buf[256] = { 0 };
+
+    while (fgets(buf, sizeof(buf), fp1) != NULL) {
+    }
+
+    char buf1[256] = { 0 };
+    char buf2[256] = { 0 };
+
+    char* ret1 = fgets(buf1, sizeof(buf1), fp1);
+    char* ret2 = fgets(buf2, sizeof(buf2), fp2);
+
+    printf("fp1 %s %p %d %d\n", buf1, ret1, ferror(fp1), feof(fp1));
+    printf("fp2 %s %p %d %d\n", buf2, ret2, ferror(fp2), feof(fp2));
+
+    fclose(fp1);
+    fclose(fp2);
 
     return 0;
 }
